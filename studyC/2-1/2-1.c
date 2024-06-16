0334d73ab4eeea36cd2e3d56f5348f974e3ac3cb
Amobu0 <kdehgogo104@gmail.com>
2024-06-16 11:18:22 +0900

    ch02-1

---
+++ studyC/2-1/2-1.c
@@ -0,0 +1,14 @@
+/* 작성자: amobu0
+	제목 10과 20 더하는 프로그램
+
+#include <stdio.h> // 4행은 전처리 단계에서 처리된다.
+
+int main(void)
+{
+	printf("Be happy!"); // 문자열 출력
+
+	printf("My Friend"); // 문자열 출력
+
+	return 0;
+
+}*/
---
+++ studyC/2-1/2-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+int main(void)
+{
+	printf("Be happy\n");
+	printf("1234567891234567890\n");
+	printf("My\tfreind\n");
+
+	printf("Goot\bd\tChance\n");
+	printf("Cow\rW\a\n");
+
+	return 0;
+}*/
