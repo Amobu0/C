3cb4b50685d87cab581493c62ad675169f49b2fb
Amobu0 <kdehgogo104@gmail.com>
2024-06-18 22:39:34 +0900

    ch04 finish

---
+++ studyC/4-1/test2-2.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int seats = 70;
+//	int audience = 65;
+//	double rate = ((double)audience / (double)seats) *100;
+//
+//	printf("ÀÔÀå·ü : %.1lf", rate);
+//
+//	return 0;
+//}
---
+++ studyC/4-1/test2-3.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int hour, min, sec;
+//	double time = 3.76;
+//
+//	hour = (int)time;
+//	min = time / 3.0;
+//
+//	printf("%.2lf시간은 %d시간 %d분 %d초입니다.", time, hour, min, sec);
+//
+//	return 0;
+//
+//}
