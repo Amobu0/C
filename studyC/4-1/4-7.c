a7bc37c13c3be95a5b59c6b1295badea01387402
Amobu0 <kdehgogo104@gmail.com>
2024-06-18 12:54:39 +0900

    ch04-2

---
+++ studyC/4-1/4-7.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int a = 10, b = 20, res;
+//
+//	a + b; //연산 결과는 바로 버려짐
+//
+//	printf("%d + %d = %d\n", a, b, a + b); //연산 결과를 바로 출력에 사용
+//
+//	res = a + b;
+//	printf("%d + %d = %d\n", a, b, res);
+//
+//
+//	return 0;
+//
+//}
---
+++ studyC/4-1/test-3.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int kor, eng, mat;
+	int res;
+	double kscore, escore, mscore, grade;
+
+
+
+	printf("3과목의 학점을 입력하세요 : ");
+	scanf("%d %d %d", &kor, &eng, &mat);
+	int tot = kor + eng + mat;
+
+	printf("3과목의 평점을 입력하세요 : ");
+	scanf("%lf %lf %lf", &kscore, &escore, &mscore);
+	grade = kscore + escore + mscore;
+	res = (tot >= 10) && (grade/3.0 > 4.0);
+	
+	printf("%d\n",res);
+
+	return 0;
+}
