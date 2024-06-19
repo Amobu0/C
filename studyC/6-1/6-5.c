f09174191295973c8391d1bee56f1cfedcdd5a8c
Amobu0 <kdehgogo104@gmail.com>
2024-06-19 22:05:47 +0900

    ch06 modify

--- studyC/6-1/6-5.c
+++ studyC/6-1/6-5.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-
-int main(void)
-{
-
-	int sum = 0;
-	int i;
-	for (i = 1; i <= 10; i++) {
-		sum += i;
-		if (sum > 30) {
-			break;
-		}
-		
-	}
-	printf("누적한 값 : %d\n", sum);
-	printf("마지막으로 더한 값 : %d\n", i);
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//
+//	int sum = 0;
+//	int i;
+//	for (i = 1; i <= 10; i++) {
+//		sum += i;
+//		if (sum > 30) {
+//			break;
+//		}
+//		
+//	}
+//	printf("누적한 값 : %d\n", sum);
+//	printf("마지막으로 더한 값 : %d\n", i);
+//	return 0;
+//}
