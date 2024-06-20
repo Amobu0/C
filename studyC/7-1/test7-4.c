74a72b643e4f6ed210f490acb231f1114b9862f6
Amobu0 <kdehgogo104@gmail.com>
2024-06-20 21:50:59 +0900

    ch07 finish

---
+++ studyC/7-1/test7-4.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+//
+//int sum(int a);
+//
+//int main(void)
+//{
+//	sum(10);
+//	sum(100);
+//	return 0;
+//}
+//
+//int sum(int a) {
+//	int tot = 0;
+//	for (int i = 1; i <= a; i++) {
+//		tot += i;
+//	}
+//	printf("1부터 %d까지의 합은 %d입니다.\n", a, tot);
+//}
