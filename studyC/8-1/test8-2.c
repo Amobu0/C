f8d4654f18d76cce669fcc89040ec0c275e307be
Amobu0 <kdehgogo104@gmail.com>
2024-06-21 17:47:30 +0900

    ch09 start

---
+++ studyC/8-1/test8-2.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[80], str2[80];
+	char temp[80];
+
+	printf("두 문자열 입력 : ");
+	scanf_s("%s %s", str1, sizeof(str1), str2, sizeof(str2));
+	printf("바꾸기 전 : %s, %s\n", str1, str2);
+
+	strcpy(temp, str1);
+	strcpy(str1, str2);
+	strcpy(str2, temp);
+	printf("바꾼 후 : %s, %s\n", str1, str2);
+
+	return 0;
+}
