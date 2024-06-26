f8bcd95353754430520b0969e33fcdb19c1e7b26
Amobu0 <kdehgogo104@gmail.com>
2024-06-26 15:48:31 +0900

    ch11-01 finish

---
+++ studyC/11-1/11-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char ch1, ch2;
+
+  scanf("%c%c", &ch1, &ch2);
+  // Enter two characters consecutively
+
+  printf("[%c%c]\n", ch1, ch2);
+  // print input text
+
+  return 0;
+}
---
+++ studyC/11-1/11-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int ch;
+
+  ch = getchar();
+  printf("Characters entered : ");
+  putchar(ch);
+  putchar('\n');
+
+  return 0;
+}
